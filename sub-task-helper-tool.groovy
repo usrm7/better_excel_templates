fa2942433dd9a74a0295a12a152eab53064ce450
usrm7 <85717140+usrm7@users.noreply.github.com>
2021-06-23 00:43:08 -0600

    Create sub-task-helper-tool.groovy

---
+++ sub-task-helper-tool.groovy
@@ -0,0 +1,22 @@
+/*
+Get subtasks and issues
+Source: https://www.midori-global.com/products/better-excel-exporter-for-jira/server/documentation/recipes
+*/
+
+subTaskHelper = new SubTaskHelperTool()
+
+public class SubTaskHelperTool {
+	/**
+	 * Returns the passed issues merged with their sub-tasks.
+	 */
+	public getIssuesAndSubtasks(issues) {
+		def issuesAndSubtasks = []
+		for(issue in issues) {
+			issuesAndSubtasks.add(issue)
+			for(subTask in issue.subTaskObjects) {
+				issuesAndSubtasks.add(subTask)
+			}
+		}
+		return issuesAndSubtasks
+	}
+}
