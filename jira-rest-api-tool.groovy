ae608ab2468a7d065612aaa4249a8abd8243d40d
usrm7 <85717140+usrm7@users.noreply.github.com>
2021-06-23 00:49:48 -0600

    Update jira-rest-api-tool.groovy
    added external api example

--- jira-rest-api-tool.groovy
+++ jira-rest-api-tool.groovy
@@ -6,6 +6,7 @@ Source: https://www.midori-global.com/products/better-excel-exporter-for-jira/se
 import groovy.json.JsonSlurper
 import org.apache.commons.io.IOUtils
 
+// your Jira server
 def user = "admin"
 def password = "admin"
 def urlConnection = new URL("http://jira.acme.com/rest/api/2/issue/DEMO-1").openConnection()
@@ -13,3 +14,7 @@ urlConnection.setRequestProperty("Authorization", "Basic " + (user + ":" + passw
 def jsonString = IOUtils.toString(urlConnection.inputStream)
 
 issueRest = new JsonSlurper().parseText(jsonString)
+
+// Use for accessing external REST APIs
+def jsonSlurper = new JsonSlurper()
+dataRest = jsonSlurper.parseText(new URL("http://services.groupkt.com/country/get/iso2code/US").text)
