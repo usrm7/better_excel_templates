d5f9a6963ff0b05cbcf33549902578d8664434ff
usrm7 <85717140+usrm7@users.noreply.github.com>
2021-06-23 00:31:48 -0600

    changed comment to the top

--- database-tool.groovy
+++ database-tool.groovy
@@ -1,13 +1,14 @@
-import com.atlassian.jira.component.ComponentAccessor
-import groovy.sql.Sql
-import org.apache.log4j.Logger
-
-database = new DatabaseTool()
 /*
 Use to return data from Jira or external databases
 
 Source: https://www.midori-global.com/products/better-excel-exporter-for-jira/server/documentation/recipes
 */
+
+import com.atlassian.jira.component.ComponentAccessor
+import groovy.sql.Sql
+import org.apache.log4j.Logger
+
+database = new DatabaseTool()
 
 class DatabaseTool {
 	def log = Logger.getLogger(this.getClass())
